38b2e46f394d0ea1c27d45d62e86cd7c1b4a3e3e
LINYAO HAN <han.linya@northeastern.edu>
2025-10-15 17:11:36 -0400

    main

---
+++ TeamGreeting/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hanlinyao
+ */
+public class Main {
+    public static void main(String[] args) {
+        LinyaoHan.main(args);
+        YilinWang.main(args);
+        Parker.main(args);
+        
+    }
+}
